22a1ac14df753eb4385eb85377326b2bddd3b4c5
SohanSomaya <sohansomaya@gmail.com>
2026-03-10 22:54:54 -0400

    storage.py updated

--- storage.py
+++ storage.py
@@ -7,8 +7,8 @@ def load_tasks():
     if not os.path.exists(FILE):
         return[]
     with open(FILE, 'r') as f:
-        return json.load(f)                 '''json.load converts JSON to python list'''
+        return json.load(f)                 #json.load converts JSON to python list#
 
 def save_tasks(tasks):
     with open(FILE, 'w') as f:
-        json.dump(tasks, f, indent=2)       '''json.dump converts python list to JSON'''
+        json.dump(tasks, f, indent=2)       #json.dump converts python list to JSON#
